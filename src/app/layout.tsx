c8ac5108600b1e3b6ef8ef738b780e8ab5f2a734
Saurabh2619 <sharmasaurabh2606@gmail.com>
2025-05-02 13:39:51 +0530

    layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,11 @@
-export default function Layout(){
-return (
-  <>
-  Layout Page of Guru Mantra Hello Hello
-  </>
-);
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en">
+      <body>
+        {/* Optional: your own layout content */}
+        <div>Layout Page of Guru Mantra Hello Hello sir</div>
+        {children}
+      </body>
+    </html>
+  );
 }
