c92062bf4dea9ecfa3d7b9663c865a4b8d6137f5
Saurabh2619 <sharmasaurabh2606@gmail.com>
2025-05-12 14:51:44 +0530

    chek

--- src/app/mba/page.tsx
+++ src/app/mba/page.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function MBA(){
   return(
     <>
-    <h6 className="text-primary text-2xl font-semibold">Hello Sir, MBA Here</h6>
+    <h6 className="text-primary text-2xl font-semibold">Hello Sir, MBA Here</h6> 
     
     </>
   )
