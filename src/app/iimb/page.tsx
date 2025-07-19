0d1082462bda78e10eb4e55de9a4a2ea3cc0e35e
Saurabh2619 <sharmasaurabh2606@gmail.com>
2025-07-19 14:05:50 +0530

    utube video iimb

--- src/app/iimb/page.tsx
+++ src/app/iimb/page.tsx
@@ -311,7 +311,7 @@ export default function IPM() {
                 <iframe
                   width="100%"
                   height="100%"
-                  src="https://www.youtube.com/live/2C1ZXLdfMfw?si=_QwGyLKfrYQa7fLx"
+                  src="https://www.youtube.com/embed/2C1ZXLdfMfw"
                   title="YouTube video player"
                   frameBorder="0"
                   allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
